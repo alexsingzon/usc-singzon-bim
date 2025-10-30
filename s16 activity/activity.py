42a1fcf21d3fe3117762f9f248e0d28cc679516c
alexsingzon <19104363@usc.edu.ph>
2025-10-30 23:14:59 +0800

    S16 Activity - Revised

---
+++ s16 activity/activity.py
@@ -0,0 +1,32 @@
+import ifcopenshell
+ifc_file = ifcopenshell.open("AC20-FZK-Haus.ifc")
+print("IFC file loaded:", ifc_file.schema)
+
+project = ifc_file.by_type("IfcProject")[0] 
+project_name = project.Name or "Unnamed Project"
+print(f"Project: {project_name}")
+
+stairs = ifc_file.by_type("IFCStair")
+print(f"Number of Stairs: {len(stairs)}")
+for stair in stairs:
+	print(stair.GlobalId, stair.Name)
+
+slabs = ifc_file.by_type("IFCSlab")
+print(f"Number of Slabs: {len(slabs)}")
+for slab in slabs:
+	print(slab.GlobalId, slab.Name)
+slabs = ifc_file.by_type("IFCSlab")
+for i, slab in enumerate(slabs, 1):
+	slab.Name = f"Renamed_Slab{i}"
+	print(f"Updated Slab: {slab.GlobalId} → {slab.Name}")
+
+project = ifc_file.by_type("IFCProject")[0]
+for site in project.IsDecomposedBy[0].RelatedObjects:
+	for building in site.IsDecomposedBy[0].RelatedObjects:
+		for storey in building.IsDecomposedBy[0].RelatedObjects:
+			print("Storey", storey.Name, "Elevation", storey.Elevation)
+
+types = set(el.is_a() for el in ifc_file)
+print("Object Types in File", types)
+
+ifc_file.write("AC20-FZK-Haus_Renamed.ifc")
